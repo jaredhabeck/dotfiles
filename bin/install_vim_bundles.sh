7c0c762d670bd8e441d34ca2f2d5bca78669cdbd
Jared Habeck <jhabeck@gmail.com>
2013-02-07 10:59:17 -0500

    update bundle installer

--- bin/install_vim_bundles.sh
+++ bin/install_vim_bundles.sh
@@ -13,26 +13,25 @@ get_bundle() {
   )
 }
 
-# I don't know what 90% of these are, so I shouldn't be installing them.
 #get_bundle adamlowe vim-slurper
 #get_bundle duff vim-bufonly
-#get_bundle godlygeek tabular
+get_bundle godlygeek tabular
 #get_bundle kchmck vim-coffee-script
-#get_bundle leshill vim-json
-#get_bundle mileszs ack.vim
+get_bundle leshill vim-json
+get_bundle mileszs ack.vim
 #get_bundle pangloss vim-javascript
 #get_bundle therubymug vim-pyte
 #get_bundle tpope vim-abolish
 #get_bundle tpope vim-bundler
 #get_bundle tpope vim-commentary
 #get_bundle tpope vim-cucumber
-#get_bundle tpope vim-endwise
+get_bundle tpope vim-endwise
 get_bundle tpope vim-eunuch
 get_bundle tpope vim-fugitive
 #get_bundle tpope vim-git
 get_bundle tpope vim-haml
 #get_bundle tpope vim-markdown
-#get_bundle tpope vim-pathogen
+get_bundle tpope vim-pathogen
 #get_bundle tpope vim-rake
 #get_bundle tpope vim-ragtag
 get_bundle tpope vim-rails
